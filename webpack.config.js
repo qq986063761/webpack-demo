6577f646512e1597433298cc04eb79fbfc3dae1a
WanPeng <986063761@qq.com>
2021-04-05 16:56:24 +0800

    更新

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,31 @@ const path = require('path')
 
 module.export = {
   mode: 'production', // development
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js'
+  },
   output: {
-    path: path.join(__dirname, 'dist')
-  }
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  // 主要负责编译文件
+  module: {
+    rules: [
+      // babel 用于编译 es6 成 es5
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  // 负责额外处理文件
+  plugins: [
+
+  ]
 }
